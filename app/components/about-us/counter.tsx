d477ca7524d8b365b7ee4d08d93112095f971da6
sumitnegii <negisumit308@gmail.com>
2026-01-02 18:09:20 +0530

    add about-us and contact-us page

--- app/components/about-us/counter.tsx
+++ app/components/about-us/counter.tsx
@@ -1,8 +1,54 @@
+// "use client";
+// import { useEffect, useState } from "react";
+
+// export default function Counter({ end, suffix = "" }) {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     let start = 0;
+//     const duration = 800;
+//     const stepTime = 20;
+//     const totalSteps = duration / stepTime;
+//     const increment = Math.ceil(end / totalSteps);
+
+//     const interval = setInterval(() => {
+//       start += increment;
+//       if (start >= end) {
+//         setCount(end);
+//         clearInterval(interval);
+//       } else {
+//         setCount(start);
+//       }
+//     }, stepTime);
+
+//     return () => clearInterval(interval);
+//   }, [end]);
+
+//   // format number (8000 → 8K)
+//   const display =
+//     end >= 1000 && count === end
+//       ? `${Math.floor(end / 1000)}K${suffix}`
+//       : count;
+
+//   return (
+//     <h1 className="font-bold text-4xl mb-1">
+//       {display}
+//     </h1>
+//   );
+// }
+
+
+
 "use client";
 import { useEffect, useState } from "react";
 
-export default function Counter({ end, suffix = "" }) {
-  const [count, setCount] = useState(0);
+type CounterProps = {
+  end: number;
+  suffix?: string;
+};
+
+export default function Counter({ end, suffix = "" }: CounterProps) {
+  const [count, setCount] = useState<number>(0);
 
   useEffect(() => {
     let start = 0;
@@ -24,7 +70,6 @@ export default function Counter({ end, suffix = "" }) {
     return () => clearInterval(interval);
   }, [end]);
 
-  // format number (8000 → 8K)
   const display =
     end >= 1000 && count === end
       ? `${Math.floor(end / 1000)}K${suffix}`
