d10a8fab72fa1fd55ed30d671f6ce02e8335dfc9
sumitnegii <negisumit308@gmail.com>
2026-01-05 14:39:43 +0530

    add login and signup page, fixed about page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
+import { ReactNode } from "react";
 
-export default function AuthLayout({ children }) {
+export default function AuthLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <main className="relative min-h-screen flex items-center justify-center">
-      
-      {/* Logo - top left */}
+      {/* Logo top-left */}
       <div className="absolute top-6 left-6">
         <Image
           src="/images/navbarLogo.png"
           alt="Logo"
-          width={100}
-          height={200}
+          width={120}
+          height={40}
         />
       </div>
 
-      {/* Auth content */}
       {children}
     </main>
   );
